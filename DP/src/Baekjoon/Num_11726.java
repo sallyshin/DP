9764af44d33442c6cb62ef84c921569aa236e657
sallyshin <46617568+sallyshin@users.noreply.github.com>
2020-03-23 21:25:51 +0900

    memoization + bottom-up

---
+++ DP/src/Baekjoon/Num_11726.java
@@ -0,0 +1,36 @@
+package Baekjoon;
+import java.io.*;;
+
+public class Num_11726 {
+	
+	public static int tile(int n) {
+		
+		int[] ar = new int[n+1];
+		
+		ar[1] = 1;
+		ar[2] = 2;
+		
+		for(int i = 3; i < n+1; i++) {
+			if(ar[i] > 0) return ar[i];
+			else {
+				ar[i] = (ar[i-1]+ ar[i-2]) % 10007;
+			}
+		}
+		
+		return ar[n];
+	}
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+
+		bw.write(String.valueOf("\n" + tile(n)));
+		bw.flush();
+		bw.close();
+
+	}
+
+}
