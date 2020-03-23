59e72b9ca18c8d59a9584c0260f5de8fbc2b58b8
sallyshin <46617568+sallyshin@users.noreply.github.com>
2020-03-24 02:14:22 +0900

    RGB거리

---
+++ DP/src/Baekjoon/Num_1149.java
@@ -0,0 +1,35 @@
+package Baekjoon;
+import java.io.*;
+
+public class Num_1149 {
+	
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		int total = 0;
+		int min = 0;
+		
+		int[][] arr = new int[n+1][3];
+		
+		for(int i = 1; i <= 3; i++) {
+			
+			
+
+		}
+		
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < n+1; j++) {
+				
+				
+			}
+		}
+		
+		bw.write(String.valueOf(total));
+		bw.flush();
+		bw.close();
+	}
+
+}
