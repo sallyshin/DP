680cb15efde6ba26142cd2596e62ff38434356f8
sallyshin <46617568+sallyshin@users.noreply.github.com>
2020-03-24 23:57:40 +0900

    RGB거리

--- DP/src/Baekjoon/Num_1149.java
+++ DP/src/Baekjoon/Num_1149.java
@@ -8,7 +8,7 @@ public class Num_1149 {
 
 		int n = sc.nextInt();
 		int[][] ar = new int[n+1][3];
-		int[][] p = new int[n+1][3];
+		int[][] p = new int[n+1][3]; // 더한 값
 
 		for(int i = 1; i < n+1; i++) {
 
@@ -17,20 +17,16 @@ public class Num_1149 {
 				ar[i][j] = sc.nextInt();
 
 			}
-			sc.nextLine();
 		}
-		
 		p[0][0] = p[0][1] = p[0][2] = ar[0][0] = ar[0][1] = ar[0][2] = 0;
-		
 		for(int i = 1; i < n+1; i++) {
-			
-			p[i][0] = ar[i][0] + Math.min(p[i-1][1], p[i][2]) + ar[i][0];
-			p[i][1] = ar[i][0] + Math.min(p[i-1][0], p[i-1][2]) + ar[i][1];
-			p[i][1] = ar[i][0] + Math.min(p[i-1][0], p[i-1][1]) + ar[i][2];
-			
+			p[i][0] = ar[i][0] + Math.min(p[i-1][1], p[i-1][2]); // 빨간색을 칠했을 때
+			p[i][1] = ar[i][1] + Math.min(p[i-1][0], p[i-1][2]); // 초록색을 칠했을 때
+			p[i][2] = ar[i][2] + Math.min(p[i-1][0], p[i-1][1]); // 파란색을 칠했을 때
 		}
 		
-		System.out.println(Math.min(p[n][2], Math.min(p[n][1], p[n][0])));
+		System.out.println(Math.min(p[n][0], Math.min(p[n][1], p[n][2])));
+		
+		
 	}
-
 }
